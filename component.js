92a92943142d90304e81e44146c9f59d148685e1
ilker <bnilker@gmail.com>
2024-06-24 15:01:32 +0300

    deg

--- component.js
+++ component.js
@@ -14,7 +14,7 @@ function create3DSlider(container) {
     </div>
   `;
 
-  var radius = 240; // how big of the radius
+  var radius = 400; // how big of the radius
   var autoRotate = true; // auto rotate or not
   var rotateSpeed = -60; // unit: seconds/360 degrees
   var imgWidth = 120; // width of images (unit: px)
