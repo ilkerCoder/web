9cb9a96dd5b7f4fa1251cf76428fd42cabd370af
ilker <bnilker@gmail.com>
2024-06-26 16:04:15 +0300

    changes

--- 3d_slider.js
+++ 3d_slider.js
@@ -5,18 +5,38 @@ let isDragging = false;
 const banner = document.querySelector(".slider");
 
 // Fare tıklama olayını dinleyin
-banner.addEventListener("mousedown", (e) => {
-  startX = e.clientX;
+banner.addEventListener("mousedown", startDragging);
+banner.addEventListener("touchstart", startDragging);
+
+function startDragging(e) {
+  e.preventDefault(); // Varsayılan işlemi engelle
+
+  if (e.type === "mousedown") {
+    startX = e.clientX;
+  } else if (e.type === "touchstart") {
+    startX = e.touches[0].clientX; // İlk dokunulan noktanın konumunu al
+  }
+
   isDragging = true;
 
   // Animasyon döngüsünü durdurun
   cancelAnimationFrame(animationLoop);
-});
+}
 
 // Fare hareketi olayını dinleyin
-banner.addEventListener("mousemove", (e) => {
+banner.addEventListener("mousemove", dragBanner);
+banner.addEventListener("touchmove", dragBanner);
+
+function dragBanner(e) {
   if (isDragging) {
-    const mouseX = e.clientX;
+    let mouseX;
+
+    if (e.type === "mousemove") {
+      mouseX = e.clientX;
+    } else if (e.type === "touchmove") {
+      mouseX = e.touches[0].clientX; // İlk dokunulan noktanın konumunu al
+    }
+
     const sensitivity = 0.5; // Hareket hassasiyeti
 
     // Hareket mesafesine göre rotateY değerini hesaplayın
@@ -32,20 +52,26 @@ banner.addEventListener("mousemove", (e) => {
     // Başlangıç noktasını güncelleyin
     startX = mouseX;
   }
-});
+}
 
 // Fare bırakma olayını dinleyin
-banner.addEventListener("mouseup", () => {
+banner.addEventListener("mouseup", stopDragging);
+banner.addEventListener("touchend", stopDragging);
+
+function stopDragging() {
   isDragging = false;
-  console.log("mouse up oldu");
+
   // Animasyon döngüsünü tekrar başlatın
   animateRotateY();
-});
+}
 
 // Fare hareketi olayını izleme
 let animationLoop;
 function animateRotateY() {
-  currentRotateY += 1; // Her adımda rotateY değerini artırın
+  currentRotateY += 0.2; // Her adımda rotateY değerini artırın
+
+  // RotateY değerini 0 ile 360 arasında döngü yaparak sınırlayın
+  currentRotateY = currentRotateY % 360;
 
   // Şu anda uygulanan transform değerini alın
   let transformValue = `perspective(500vw) rotateX(-16deg) rotateY(${currentRotateY}deg)`;
